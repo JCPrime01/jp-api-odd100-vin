85cc448c1067f990e9a780eb2da94d6f521ba91b
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-28 21:43:50 -0300

    chore: update shareCode to NAP0SIAYUWH

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=9YLY6ETXNQ0&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=NAP0SIAYUWH&afp=tapi100' });
     res.end();
   }
